6bf696d5616f4e9ee57726810b2888629afe59c8
wdtcw <wdtcw@DESKTOP-L0MOCUM>
2020-01-05 16:13:33 +0900

    DataSouce, MyBatis 연동 TEST 성공

---
+++ src/test/java/kr/co/web/MyBatisTest.java
@@ -0,0 +1,32 @@
+package kr.co.web;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//Runner 클래스(테스트 메소드를 실행하는 클래스) 를 SpringJUnit4ClassRunner로 함
+@RunWith(SpringJUnit4ClassRunner.class)
+//location 속성 경로에 있는 xml 파일을 이용해서 스프링이 로딩됨
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
+public class MyBatisTest {
+	
+	//SqlSessionFactory 객체를 자동으로 완성
+	@Inject
+	private SqlSessionFactory sqlFactory;
+	
+	//SqlSesstionFactory 객체가 제대로 만들어졌는지 test
+	@Test
+	public void testSession() throws Exception {
+		try(SqlSession session = sqlFactory.openSession()){
+			System.out.println(session);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
